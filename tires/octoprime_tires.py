5272d67dd60698352a4baf99729c9b28ad39fcd8
Pratish Ninawe <pratish.1006@gmail.com>
2023-05-10 16:20:34 +0530

    task 4 reworked

--- tires/octoprime_tires.py
+++ tires/octoprime_tires.py
@@ -3,7 +3,7 @@ from tires.tires import Tires
 class OctoprimeTires(Tires):
     def __init__(self,tire_wear):
         self.tire_wear = tire_wear
-        self.tire_service = 3
+        self.tire_service = 3.0
 
     def needs_service(self):
         return sum(self.tire_wear) >= self.tire_service
