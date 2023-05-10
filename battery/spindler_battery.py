884de6d2f14d631d53aca5af0cf44e77152a7a59
Pratish Ninawe <pratish.1006@gmail.com>
2023-05-10 16:19:08 +0530

    task 4

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -8,9 +8,9 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
 
     def battery_should_be_serviced(self):
-        two_yrs = datetime.timedelta(days=365*2)
+        three_yrs = datetime.timedelta(days=365*3)
         diff = self.current_date - self.last_service_date
-        if diff >= two_yrs:
+        if diff >= three_yrs:
             return True
         else:
             return False
